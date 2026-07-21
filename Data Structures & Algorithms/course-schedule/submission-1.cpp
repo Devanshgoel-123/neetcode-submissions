d2cab5dabe6d5f8156e1024edafa2707d761ecd9
Devansh Goel <117383704+Devanshgoel-123@users.noreply.github.com>
2026-07-21 20:16:58 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        vector<vector<int>> adjList(n);
+        vector<int> cycleDetection;
+        for (auto &p : prerequisites) {
+            int course = p[0];
+            int preReq = p[1];
+            adjList[course].push_back(preReq);
+        }
+        vector<int> inDeg = vector<int>(n,0);
+        for(int i=0; i < n; i++) {
+            for( auto it: adjList[i]) {
+                inDeg[it]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0; i < n; i++) {
+            if(!inDeg[i]) q.push(i);
+        }
+
+        while(!q.empty()) {
+            int node = q.front();
+            for(auto it: adjList[node]) {
+                inDeg[it]--;
+                if(!inDeg[it]) q.push(it);
+            }
+            q.pop();
+            cycleDetection.push_back(node);
+        }
+
+        return cycleDetection.size() == numCourses;
+    }
+};
